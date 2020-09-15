670cfa02b90b4b4853452bd7fdc55c4fd98ca259
Christopher Demahy <demahyc@gmail.com>
2020-09-15 19:12:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-		programmer_hash = programmer_hash[:dennis_ritchie][:languages].unshift("Assembly")
+		programmer_hash = programmer_hash[:dennis_ritchie][:languages].shift("Assembly")
 
 		puts programmer_hash
 	end
